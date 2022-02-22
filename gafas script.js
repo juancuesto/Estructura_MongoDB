50623eeb2fb052f1d6271d81c3962c487734c979
juancuesto <66420450+juancuesto@users.noreply.github.com>
2022-02-22 12:21:02 +0100

    Add files via upload

---
+++ gafas script.js
@@ -0,0 +1,27 @@
+
+db.gafas.find()
+db.gafas.insertMany([{
+    "nombre marca":"safilo",
+    "color gafa":"verde",
+    "color lentes":"azul",
+    "pvp":139,
+    "Grad OD":"-2.00",
+    "Grad OI": "-2.50"
+},
+{
+    "nombre marca":"safilo",
+    "color gafa":"rojo",
+    "color lentes":"azul",
+    "pvp":100,
+    "Grad OD":"-1.00",
+    "Grad OI": "-1.50"
+},
+{
+    "nombre marca":"marchon",
+    "color gafa":"verde",
+    "color lentes":"azul",
+    "pvp":89,
+    "Grad OD":"-3.00",
+    "Grad OI": "-3.50"
+    }])
+    
---
+++ pizzeria script.js
@@ -0,0 +1,32 @@
+use pizzeria
+db.clientes.find()
+db.clientes.insertMany([{"nombre":"Luis",
+    "email":"Luis@hotmail.com",
+    "fecha registro":"20/06/2020",
+    "atendido por":"Antonio",
+    "cliente nuevo":"no",
+    "recomendado por":"no"},
+    {"nombre":"Ana",
+    "email":"Ana@hotmail.com",
+    "fecha registro":"20/05/2020",
+    "atendido por":"Antonio",
+    "cliente nuevo":"no",
+    "recomendado por":"no"},
+    {"nombre":"Pepi",
+    "email":"Pepi@hotmail.com",
+    "fecha registro":"20/05/2020",
+    "atendido por":"Antonio",
+    "cliente nuevo":"no",
+    "recomendado por":"no"}])
+    db.categoria.insertMany([{
+        "id_categoria":1,
+        "nombre":"pizza"
+    },
+    {
+         "id_categoria":2,
+        "nombre":"hambueguesa"
+    },{
+         "id_categoria":3,
+        "nombre":"bebida"
+    }])
+    
